715aa8228196d97a1ba44edab3756b78ec2c69b5
Micodesoft Nigeria <131521846+micodesoft@users.noreply.github.com>
2023-04-22 11:19:49 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+include_once "helpers.php";
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    
+    <title><?=APP_NAME ?></title>
+  </head>
+  
+  <body>
+    <header>
+      <h1><?=APP_NAME ?></h1>
+    </header>
+  </body>
+</html>
